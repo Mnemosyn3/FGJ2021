275065e4baeaff04cfb308b7c7158a7887ee7dd3
Mnemosyn3 <robert.hamalainen@iki.fi>
2021-01-29 23:18:09 +0200

    Moving player

--- source/movingBall.py
+++ source/movingBall.py
@@ -79,6 +79,13 @@ class Player(pygame.sprite.Sprite):
                 self.frame = 0
             self.image = self.images[self.frame//ani]
 
+         # moving down
+        if self.movey < 0:
+            self.frame += 1
+            if self.frame > 3*ani:
+                self.frame = 0
+            self.image = self.images[self.frame//ani]
+
 
 '''
 Setup
@@ -122,13 +129,19 @@ while main:
             if event.key == pygame.K_RIGHT or event.key == ord('d'):
                 player.control(steps, 0)
             if event.key == pygame.K_UP or event.key == ord('w'):
-                print('jump')
+                player.control(0,-steps)
+            if event.key == pygame.K_DOWN or event.key == ord('s'):
+                player.control(0,steps)
 
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == ord('a'):
                 player.control(steps, 0)
             if event.key == pygame.K_RIGHT or event.key == ord('d'):
                 player.control(-steps, 0)
+            if event.key == pygame.K_UP or event.key == ord('w'):
+                player.control(0,steps)
+            if event.key == pygame.K_DOWN or event.key == ord('s'):
+                player.control(0,-steps)
 
     world.blit(backdrop, backdropbox)
     player.update()
