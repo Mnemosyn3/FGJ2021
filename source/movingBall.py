7b51deaa9d9e626e34f39adc6ac7bdbb68af0e3c
Mnemosyn3 <robert.hamalainen@iki.fi>
2021-01-29 23:07:19 +0200

    Latest

--- source/movingBall.py
+++ source/movingBall.py
@@ -10,8 +10,8 @@ Variables
 
 worldx = 960
 worldy = 720
-fps = 40  # frame rate
-ani = 4  # animation cycles
+fps = 40
+ani = 4
 world = pygame.display.set_mode([worldx, worldy])
 
 BLUE = (25, 25, 200)
@@ -31,6 +31,9 @@ class Player(pygame.sprite.Sprite):
 
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
+        self.movex = 0
+        self.movey = 0
+        self.frame = 0
         self.images = []
         for i in range(1, 5):
             img = pygame.image.load(os.path.join('images', 'hero' + str(i) + '.png')).convert()
@@ -39,6 +42,42 @@ class Player(pygame.sprite.Sprite):
             self.images.append(img)
             self.image = self.images[0]
             self.rect = self.image.get_rect()
+
+    def control(self, x, y):
+        """
+        control player movement
+        """
+        self.movex += x
+        self.movey += y
+
+    def update(self):
+        """
+        Update sprite position
+        """
+
+        self.rect.x = self.rect.x + self.movex
+        self.rect.y = self.rect.y + self.movey
+
+        # moving left
+        if self.movex < 0:
+            self.frame += 1
+            if self.frame > 3*ani:
+                self.frame = 0
+            self.image = pygame.transform.flip(self.images[self.frame // ani], True, False)
+
+        # moving right
+        if self.movex > 0:
+            self.frame += 1
+            if self.frame > 3*ani:
+                self.frame = 0
+            self.image = self.images[self.frame//ani]
+        
+        # moving up
+        if self.movey > 0:
+            self.frame += 1
+            if self.frame > 3*ani:
+                self.frame = 0
+            self.image = self.images[self.frame//ani]
 
 
 '''
@@ -56,7 +95,7 @@ player.rect.x = 0  # go to x
 player.rect.y = 0  # go to y
 player_list = pygame.sprite.Group()
 player_list.add(player)
-
+steps = 10
 
 '''
 Main Loop
@@ -72,24 +111,27 @@ while main:
                 main = False
 
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT or event.key == ord('a'):
-                print('left')
-            if event.key == pygame.K_RIGHT or event.key == ord('d'):
-                print('right')
-            if event.key == pygame.K_UP or event.key == ord('w'):
-                print('jump')
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == ord('a'):
-                print('left stop')
-            if event.key == pygame.K_RIGHT or event.key == ord('d'):
-                print('right stop')
             if event.key == ord('q'):
                 pygame.quit()
-                sys.exit()
-                main = False    
+                try:
+                    sys.exit()
+                finally:
+                    main = False
+            if event.key == pygame.K_LEFT or event.key == ord('a'):
+                player.control(-steps, 0)
+            if event.key == pygame.K_RIGHT or event.key == ord('d'):
+                player.control(steps, 0)
+            if event.key == pygame.K_UP or event.key == ord('w'):
+                print('jump')
 
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == ord('a'):
+                player.control(steps, 0)
+            if event.key == pygame.K_RIGHT or event.key == ord('d'):
+                player.control(-steps, 0)
 
     world.blit(backdrop, backdropbox)
+    player.update()
     player_list.draw(world)
     pygame.display.flip()
     clock.tick(fps)
